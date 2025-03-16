cbdc832b0883649c9e0cecc9774ba6a49fd8a58d
Priyantha <kjpriyanthafdo@gmail.com>
2025-03-16 19:24:42 +0530

    add safearea

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Mainview(),
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Mainview(),
+      ),
     );
   }
 }
